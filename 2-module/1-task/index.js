d15a1846748d466561930eb1635b34e3565bc7e8
said <sidi112233445566778899@gmail.com>
2023-12-16 21:35:49 +0300

    решение задачи на суммирование зарплаты

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,16 @@
 function sumSalary(salaries) {
   // ваш код...
+  let sum = 0;
+
+  if (Object.keys(salaries).length == 0) {
+    return 0;
+  }
+
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number' && !isNaN(salaries[key]) && isFinite(salaries[key])) {
+      sum += salaries[key];
+    }
+  }
+
+  return sum;
 }
