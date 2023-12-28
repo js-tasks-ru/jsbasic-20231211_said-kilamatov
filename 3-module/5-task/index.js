cbb0d1c20ca60164811fdd3163795aa51db31c23
said <sidi112233445566778899@gmail.com>
2023-12-28 14:00:15 +0300

    Решил задачу

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,15 @@
 function getMinMax(str) {
   // ваш код...
+  let copyOfStr = str
+    .split(" ")
+    .filter((item, index, array) =>
+      typeof item === "number" ? parseInt(item) : array.shift()
+    );
+  let max = Math.max(...copyOfStr);
+  let min = Math.min(...copyOfStr);
+
+  return (result = {
+    max,
+    min,
+  });
 }
