69f7197e21154b59005d06d8ee37fabbdbdb44c8
said <sidi112233445566778899@gmail.com>
2024-01-03 21:57:56 +0300

    решил задание номер 3 модуля 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
   // ваш код...
+  let tbody = table.querySelector("tbody");
+  for (let row of tbody.rows) {
+    let td = row.children[3];
+    if (td.getAttribute("data-available") === "true") {
+      row.classList.add("available");
+    } else if (td.getAttribute("data-available") === null) {
+      row.setAttribute("hidden", "");
+    } else {
+      row.classList.add("unavailable");
+    }
+
+    td = row.children[2];
+    if (td.innerHTML === "m") {
+      row.classList.add("male");
+    } else {
+      row.classList.add("female");
+    }
+
+    td = row.children[1];
+    if (+td.innerHTML < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
