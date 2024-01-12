a4e99b965fa305f67b0cc7f8c2e8259e8594d440
said <sidi112233445566778899@gmail.com>
2024-01-12 09:26:13 +0300

    сделал задание 1 модуля 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,68 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = rows;
+  }
+
+  set elem(rows) {
+    let table = document.createElement("table");
+    let thead = document.createElement("thead");
+    table.prepend(thead);
+
+    let trHead = document.createElement("tr");
+    thead.append(trHead);
+    let uniqueKeys = [];
+
+    for (let i = 0; i < this.rows.length; i++) {
+      let keys = Object.keys(this.rows[i]);
+
+      keys.map((key) => {
+        if (!uniqueKeys.includes(key)) {
+          uniqueKeys.push(key);
+          let th = document.createElement("th");
+          th.textContent = key;
+          trHead.append(th);
+        }
+      });
+    }
+
+    let tbody = document.createElement("tbody");
+    table.append(tbody);
+
+    for (let i = 0; i < this.rows.length; i++) {
+      let tr = document.createElement("tr");
+      tbody.append(tr);
+
+      let value = Object.values(this.rows[i]);
+      value.map((value) => {
+        let td = document.createElement("td");
+        td.textContent = value;
+        tr.append(td);
+      });
+
+      tr.insertAdjacentHTML("beforeend", "<td><button>X</button></td>");
+    }
+
+    let tr = document.querySelectorAll("tr");
+
+    table.onclick = function (event) {
+      let target = event.target;
+
+      if (target.tagName != "BUTTON") return;
+
+      let tr = target.closest("tr");
+
+      if (tr) {
+        tr.remove();
+      }
+    };
+    this.table = table;
+    document.body.append(table);
+    return table;
+  }
+
+  get elem() {
+    return this.table;
   }
 }
