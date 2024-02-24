dca8ec197f828170bfb112fe4b2a7d771a3830a5
said <sidi112233445566778899@gmail.com>
2024-02-24 14:36:14 +0300

    решение задачи  номер 2 модуля 8

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -1,9 +1,55 @@
-import createElement from '../../assets/lib/create-element.js';
-import ProductCard from '../../6-module/2-task/index.js';
+import createElement from "../../assets/lib/create-element.js";
+import ProductCard from "../../6-module/2-task/index.js";
 
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.render();
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner">
+        </div>
+      </div>
+    `);
+
+    this.renderCards(this.products);
+  }
+
+  renderCards(products) {
+    const productsGridInner = this.elem.querySelector(".products-grid__inner");
+    productsGridInner.innerHTML = "";
+
+    for (let product of products) {
+      productsGridInner.append(new ProductCard(product).elem);
+    }
+  }
+
+  updateFilter(filters) {
+    Object.assign(this.filters, filters);
+
+    let products = this.products.filter((product) => {
+      switch (true) {
+        case this.filters.noNuts && product.nuts:
+          return false;
+
+        case this.filters.vegeterianOnly && !product.vegeterian:
+          return false;
+
+        case this.filters.maxSpiciness < product.spiciness:
+          return false;
+
+        case this.filters.category && product.category != this.filters.category:
+          return false;
+
+        default:
+          return true;
+      }
+    });
+
+    this.renderCards(products);
   }
 }
